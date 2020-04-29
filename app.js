e928852dcc463833d25c71c86fbcafeec4dd1eff
wangxiaoli <1377034995@qq.com>
2020-04-29 18:05:40 +0800

    master

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express'),
     //mongoose内以来自动有mongodb,所以不需要再次安装或引入mongodb
       mongoose = require('mongoose'),
       cookieSession = require('cookie-session');
+      loginCtrl = require('./controllers/loginCtrl');
 
 //连接数据库 端口不需要写，最后的反斜杠是操作的数据库名称
 mongoose.connect('mongodb://localhost/sm',{useNewUrlParser:true,useUnifiedTopology:true});
@@ -18,7 +19,9 @@ app.use(cookieSession({
     maxAge : 30 * 60 * 1000 //30min
 }))
 //路径清单:
-
+app.get('/login',loginCtrl.showLogin);//访问登陆页面 
+app.post('/login',loginCtrl.doLogin);//访问登录接口,处理登录操作
+app.propfind('/login',loginCtrl.checkUser);//访问接口 验证用户名是否存在
 
 //处理静态资源请求:
 app.use(express.static('public'));
